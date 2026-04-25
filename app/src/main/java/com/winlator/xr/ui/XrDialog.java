a70b8ece88f9d7b775b3ea74e83e69866aeafe08
Lubos <tridosm@gmail.com>
2026-04-25 16:23:24 +0200

    Do not show Reshade in the quick menu yet

--- app/src/main/java/com/winlator/xr/ui/XrDialog.java
+++ app/src/main/java/com/winlator/xr/ui/XrDialog.java
@@ -60,7 +60,7 @@ public class XrDialog extends ContentDialog {
         addMenuItem(activity, grid, R.drawable.icon_task_manager, R.string.task_manager, MenuItem.TASK_MANAGER, 1.0f);
         addMenuItem(activity, grid, R.drawable.icon_magnifier, R.string.magnifier, MenuItem.WINDOW_SCALE, 1.0f);
         if (XrActivity.getFlag(XrActivity.Flag.RESHADE)) {
-            addMenuItem(activity, grid, R.drawable.icon_popup_menu_bring_to_front, R.string.reshade, MenuItem.RESHADE_MENU, 1.0f);
+            //addMenuItem(activity, grid, R.drawable.icon_popup_menu_bring_to_front, R.string.reshade, MenuItem.RESHADE_MENU, 1.0f);
         }
         addMenuItem(activity, grid, R.drawable.icon_exit, R.string.exit_game, MenuItem.EXIT_GAME, 1.0f);
 
@@ -152,7 +152,7 @@ public class XrDialog extends ContentDialog {
         icon.setLayoutParams(lp);
         layout.addView(icon);
 
-        int width = dpToPx(96, context);
+        int width = dpToPx(80, context);
         TextView text = new TextView(context);
         text.setLayoutParams(new ViewGroup.LayoutParams(width, ViewGroup.LayoutParams.WRAP_CONTENT));
         text.setText(context.getString(titleRes));
